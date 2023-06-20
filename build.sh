2249173dab440ca4e4448766f5fc2f7ed1056f2e
Thales Vargas <101616134+ThalesTayson@users.noreply.github.com>
2023-06-20 00:41:31 -0400

    corrigindo bug

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ poetry install
 poetry run python manage.py collectstatic --no-input
 poetry run python manage.py makemigrations
 poetry run python manage.py migrate
-poetry run python python init_base.py
+poetry run python init_base.py
